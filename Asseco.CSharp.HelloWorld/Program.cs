36e24e4340288f80b1e1f6c6cd8deedd83546169
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-09-25 09:33:06 +0200

    Add project files.

---
+++ Asseco.CSharp.HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asseco.CSharp.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
